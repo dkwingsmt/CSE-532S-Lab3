cdc6e065158991f8d78bce8740827cbad3e80d59
Tong Mu <dkwingsmt@gmail.com>
2015-11-11 15:31:35 -0600

    Testmain on macbeth

--- lab3client/test_main.sh
+++ lab3client/test_main.sh
@@ -1,1 +1,2 @@
-make main && build/lab2 configs/partial_hamlet_act_ii_script.txt
+#make main && build/lab2 ../configs/partial_hamlet_act_ii_script.txt
+make main && build/lab2 ../configs/partial_macbeth_act_i_script.txt
