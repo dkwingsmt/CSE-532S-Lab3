4cadbd290da617a53d35877a40994a48a7fc84fd
Tong Mu <dkwingsmt@gmail.com>
2015-11-15 18:39:30 -0600

    Delegate everything of director to Script class

--- lab3client/lab2/Director.h
+++ lab3client/lab2/Director.h
@@ -1,43 +1,15 @@
 #ifndef __DIRECTOR_H__
 #define __DIRECTOR_H__
 
-#include <string>
-#include <fstream>
-#include <list>
-#include <utility>
-#include <mutex>
-#include <memory>
-#include <condition_variable>
-#include <stdexcept>
-#include <atomic>
-
-#include "common.h"
-#include "Player.h"
-#include "Play.h"
+#include "Script.h"
 
 class Director {
 private:
-    const static std::string PREFIX_SCENE;
-    std::vector<tFragConfig> _scriptConfig;
-    std::shared_ptr<Play> _play;
-    std::list<std::shared_ptr<Player>> _players;
-
-    std::atomic<Player *> _idler;
-    std::atomic<bool> _hasDirector;
-
-    // Returns biggestPairFrags
-    size_t _readScript(std::string &scriptFileName);
-    bool _readFragConfig(std::ifstream &, 
-                std::list<tCharConfig> &, 
-                std::string);
-    void _recruit(size_t numPlayers);
-
+    Script _script;
 public:
     Director(std::string scriptFileName, size_t numberOfPlayers = 0, bool bOverride = false) : 
-		_idler(NULL), _hasDirector(false)
+        _script(this, scriptFileName, numberOfPlayers, bOverride)
     {
-        size_t biggestPairFrags = _readScript(scriptFileName);
-        _recruit(bOverride ? numberOfPlayers : std::max(biggestPairFrags, numberOfPlayers));
     }
 
 	~Director() {
@@ -45,15 +17,23 @@ public:
 	}
 
     // Called by the now-director Player
-    void cue(size_t fragId, tCharConfig charConfig);
+    void cue(size_t fragId, tCharConfig charConfig) {
+        _script.cue(fragId, charConfig);
+    }
 
     // Must call Director::ended() after this function completes!
-	bool actEnded() { return _play->actEnded(); }
+	bool actEnded() { return _script.actEnded(); }
 
-    void declareIdle(Player *me);
-    bool electDirector();
+    void declareIdle(Player *me) {
+        return _script.declareIdle(me);
+    }
+
+    bool electDirector() {
+        return _script.electDirector();
+    }
+
 	void resign() {
-		_hasDirector = false;
+        _script.resign();
 	}
     
 };
---
+++ lab3client/lab2/Script.cpp
@@ -0,0 +1,112 @@
+#include <sstream>
+#include "Script.h"
+#include "common.h"
+
+using namespace std;
+
+const string Script::PREFIX_SCENE = "[scene]";
+
+size_t Script::_readScript(string &scriptFileName) {
+    ifstream scriptFile(scriptFileName);
+    if (!scriptFile) {
+        throw invalid_argument(scriptFileName);
+    }
+
+    string line;
+    string scene_title;
+    string dir = dirnameOf(scriptFileName);
+    size_t biggestPairFrags = 0;
+    size_t sizeLastFrag = 0;
+    while (getline(scriptFile, line)) {
+        if (line[0] == '[') {
+            if (line.substr(0, PREFIX_SCENE.size()) != PREFIX_SCENE) {
+                throw invalid_argument(line);
+            }
+            scene_title = trim(line.substr(PREFIX_SCENE.size()));
+        }
+        else {
+            // Read fragment file
+            _scriptConfig.push_back({scene_title, {}});
+            string fragFileName = dir + trim(line);
+            ifstream fragFile(fragFileName);
+            if (!fragFile) {
+                cout << "Can't open character file " << fragFileName << endl;
+                continue;
+            }
+            _readFragConfig(fragFile, _scriptConfig.back().chars, dir);
+
+            // Refresh scene title
+            scene_title = "";
+
+            // Calculate pair-of-frags
+            size_t sizeThisFrag = _scriptConfig.back().chars.size();
+            biggestPairFrags = max(biggestPairFrags, sizeLastFrag+sizeThisFrag);
+            sizeLastFrag = sizeThisFrag;
+        }
+    }
+
+    return biggestPairFrags;
+}
+
+bool Script::_readFragConfig(ifstream &fragFile, 
+            std::list<tCharConfig> &chars, string dir) {
+	list<Player> players;
+    // Read play file
+    string line;
+    while (getline(fragFile, line))
+    {
+        line = trim(line);
+        if (line.empty())
+            continue;
+        string characterName;
+        string inputFileName;
+        if (istringstream(line) >> characterName >> inputFileName)
+        {
+            chars.push_back({characterName, dir+inputFileName});
+        }
+    }
+    return true;
+}
+
+void Script::_recruit(size_t numPlayers) {
+    _play = make_shared<Play>(_scriptConfig);
+    _players.clear();
+    for (size_t i = 0; i < numPlayers; i++) {
+        _players.push_back(make_shared<Player>(_play.get(), _director));
+    }
+}
+
+bool Script::electDirector() {
+    Player *leader;
+    // Wait until there's no director
+	while (_hasDirector) {
+		this_thread::yield();
+	}
+    // Wait until there's an idle
+	while (!(leader = atomic_exchange<Player*>(&_idler, NULL))) {
+		this_thread::yield();
+	}
+	_hasDirector = true;
+	
+	leader->assignLeader(_play->getNextTask());
+
+	return _play->distributeEnded();
+}
+
+void Script::cue(size_t fragId, tCharConfig charConfig) {
+    Player *follower;
+    while (!(follower = atomic_exchange<Player*>(&_idler, NULL))) {
+        this_thread::yield();
+    }
+	tFollowerTask task({ fragId, charConfig.first, charConfig.second });
+    follower->assignFollower(task);
+}
+
+void Script::declareIdle(Player *me) {
+	Player *empty = NULL;
+	while (!atomic_compare_exchange_strong(&_idler, &empty, me) && !_play->actEnded()) {
+		empty = NULL;
+		this_thread::yield();
+	}
+}
+
