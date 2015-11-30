1e6164da873126a551fda34273187c4cefd9bdb3
zjx <zhao_j1@denison.edu>
2015-11-30 11:51:08 -0600

    add function getScriptsAndStatus

--- director/Director.h
+++ director/Director.h
@@ -58,17 +58,17 @@ public:
 	std::vector<std::string> getScriptsAndStatus()
 	{
 		std::vector<std::string>  Scripts;
-		//for (auto&& scriptName : _scriptsFileName)
-		//{
-		//	if (scriptName != _nowScript->getScriptName())
-		//	{
-		//		Scripts.push_back(scriptName + " available ");
-		//	}
-		//	else
-		//	{
-		//		Scripts.push_back(scriptName+ " inProgress ");
-		//	}
-		//}
+		for (auto&& scriptName : _scriptsFileName)
+		{
+			if (scriptName != _nowScript->getScriptName())
+			{
+				Scripts.push_back(scriptName + " available ");
+			}
+			else
+			{
+				Scripts.push_back(scriptName+ " inProgress ");
+			}
+		}
 		return Scripts;
 	}
 	
